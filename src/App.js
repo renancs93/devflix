f7980c98e655b571c2344a27fa08db35968a5ebf
Renan Costa da Silva <renancs93@gmail.com>
2020-07-27 21:08:40 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import dadosIniciais from "./data/dados_iniciais.json";
+
+import Menu from "./components/Menu";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
+  const destaque = dadosIniciais.categorias[0].videos[0];
+  const categ = dadosIniciais.categorias;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "#141414"}}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={destaque.titulo}
+        url={destaque.url}
+        videoDescription={
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        }
+      />
+
+      <Carousel ignoreFirstVideo category={categ[0]} />
+
+      <Carousel category={categ[1]} />
+
+      <Carousel category={categ[2]} />
+
+      <Carousel category={categ[3]} />
+
+      <Carousel category={categ[4]} />
+
+      <Carousel category={categ[5]} />
     </div>
   );
 }
